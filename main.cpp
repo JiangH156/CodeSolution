b1c78dd733d5be636335a0276b866e2e94ff9814
HuangJ <2360542085@qq.com>
2023-09-08 09:59:54 +0800

    init commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+typedef vector<int> VI;
+typedef pair<int,int> PII;
+typedef unsigned long long ULL;
+
+const LL N = 1e5 + 10, INF = 1<<29, M = 1e9 + 7;
+LL a[N], f[N], g[N], s[N];
+
+LL t,n,m,k,q;
+void solve()
+{
+    while(t -- )
+    {
+
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    cin >> t;
+    solve();
+    return 0;
+}
